cece64e1dc0bf209d34638037012bf6ae3ed7baa
OskarTall0 <oskar.tallo@voco.ee>
2026-01-08 13:14:50 +0200

    Ylesanne 1

---
+++ Expences/src/components/ExpenseItem.jsx
@@ -0,0 +1,20 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props) => {
+  return (
+    <div className="expense-item">
+      <div className="expense-item__date">
+        <ExpenseDate date={props.data.date} />
+      </div>
+
+      <div className="expense-item__description">
+        <h2>{props.data.title}</h2>
+      </div>
+
+      <div className="expense-item__price">{props.data.price}</div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
